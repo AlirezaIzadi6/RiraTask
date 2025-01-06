e24ecfe70703cd71d91579c66c41a4babb058e97
Alireza Izadi <alirezaizadi199912@gmail.com>
2025-01-06 13:08:43 +0330

    Change price type to double in Product model

--- RiraTask/Models/Product.cs
+++ RiraTask/Models/Product.cs
@@ -7,6 +7,6 @@ namespace RiraTask.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public Categories Category { get; set; }
-        public int Price { get; set; }
+        public double Price { get; set; }
     }
 }
